47f4d3becaca2846db3567228c67a55a9911f1a9
Alireza Saei <saeialireza359@gmail.com>
2023-10-27 17:20:31 +0330

    Initilaized files for College question

---
+++ Assginment 1/College/College/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace College
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var parts = input?.Split(" ");
+
+                switch (parts?[0])
+                {
+                    case "REGISTER":
+                        break;
+                    case "APPROVE":
+                        break;
+                    case "REJECT":
+                        break;
+                    case "QUEUE":
+                        break;
+                    case "CHANGEROLE":
+                        break;
+                    case "STATUS":
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                }
+
+            }
+        }
+    }
+}
