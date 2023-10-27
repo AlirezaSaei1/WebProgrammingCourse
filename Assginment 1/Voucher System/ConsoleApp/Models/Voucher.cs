74faa3c3854ad4c1630cf3fd31eb8baadc071e77
Alireza Saei <saeialireza359@gmail.com>
2023-10-27 16:45:19 +0330

    Added operator overloading to sum vouchers

--- Assginment 1/Voucher System/ConsoleApp/Models/Voucher.cs
+++ Assginment 1/Voucher System/ConsoleApp/Models/Voucher.cs
@@ -14,5 +14,38 @@ public class Voucher
     }
     
     // Operator Overloading
-    
+    public static Voucher operator +(Voucher v1, Voucher v2)
+    {
+        var mergedVoucher = new Voucher
+        {
+            DeliveryDate = v1.DeliveryDate > v2.DeliveryDate ? v1.DeliveryDate : v2.DeliveryDate,
+            Details = new List<VoucherSub>()
+        };
+
+        var d1 = v1.Details;
+        var d2 = v2.Details;
+        var mergedDetails = d1.Concat(d2).ToList();
+        
+        var rowNum = 1;
+        foreach (var detail in mergedDetails)
+        {
+            var existingDetail = mergedVoucher.Details.FirstOrDefault(d => d.Product.Code == detail.Product.Code);
+            if (existingDetail != null && detail.Product.IsMergeable)
+            {
+                existingDetail.Quantity += detail.Quantity;
+                existingDetail.Price = Math.Max(existingDetail.Price, detail.Price);
+            }
+            else
+            {
+                mergedVoucher.Details.Add(new VoucherSub(
+                    rowNum++,
+                    detail.Product,
+                    detail.Quantity,
+                    detail.Price
+                ));
+            }
+        }
+        mergedVoucher.TotalPrice = mergedVoucher.Details.Sum(d => d.Price * d.Quantity);
+        return mergedVoucher;
+    }
 }
