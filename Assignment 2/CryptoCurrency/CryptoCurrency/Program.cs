ad48888db55f29091222a647ebeb8e79140ab7ad
Alireza Saei <saeialireza359@gmail.com>
2023-11-03 15:53:42 +0330

    Added initial folders for Assignment 2 - CryptoCurrency

---
+++ Assignment 2/CryptoCurrency/CryptoCurrency/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CryptoCurrency
+{
+    public class Program
+    {
+        public static void main(string[] args)
+        {
+            // Line 1
+            var firstLine = Console.ReadLine().Split(' ');
+            var target = int.Parse(firstLine[0]);
+            var coinCount = int.Parse(firstLine[1]);
+            
+            // Line 2
+            var coinNames = Console.ReadLine().Split(' ');
+
+            // Line 3
+            var messageCount = int.Parse(Console.ReadLine());
+
+            // Read the messages
+            for (var i = 0; i < messageCount; i++)
+            {  
+                var command = Console.ReadLine();
+            }
+        }
+    }
+}
+
