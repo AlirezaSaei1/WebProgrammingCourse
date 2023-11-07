d75015d68debc4e8ce7863f3862aafa24032f0ff
Alireza Saei <saeialireza359@gmail.com>
2023-11-07 12:48:51 +0330

    Added a function for checking completed orders

--- Assignment 2/CryptoCurrency/CryptoCurrency/Services/OrderBookService.cs
+++ Assignment 2/CryptoCurrency/CryptoCurrency/Services/OrderBookService.cs
@@ -20,20 +20,26 @@ namespace CryptoCurrency.Services
             else
             {
                 _buyOrders[order.Coin].Add(order);
+                _buyOrders[order.Coin].Sort();
                 _totalBuy[order.Coin] += order.Size;
             }
+            CheckForCompletedOrders(order.Coin, target, "BUY");
         }
 
         public void ProcessSellOrder(Order order, int target)
         {
             if (!_sellOrders.ContainsKey(order.Coin))
             {
-                _sellOrders[order.Coin] = new List<Order>();
+                _sellOrders[order.Coin] = new List<Order>{ order };
+                _totalSell[order.Coin] = order.Size;
             }
             else
             {
-                _sellOrders[order.Coin].Add(order);    
+                _sellOrders[order.Coin].Add(order);
+                _sellOrders[order.Coin].Sort();
+                _totalSell[order.Coin] += order.Size;
             }
+            CheckForCompletedOrders(order.Coin, target, "SELL");
         }
 
         public void RemoveOrder(Order order)
@@ -49,6 +55,22 @@ namespace CryptoCurrency.Services
             else
             {
                 Console.WriteLine("ID could not be found");
+            }
+        }
+
+        private static void CheckForCompletedOrders(string coin, int target, string type)
+        {
+            if (type == "BUY")
+            {
+             
+            }
+            else if (type == "SELL")
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Invalid Type");
             }
         }
     }   
