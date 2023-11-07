dd0a389b5a7740b15aa374fe7d7e30f4f427bcd4
Alireza Saei <saeialireza359@gmail.com>
2023-11-07 22:40:59 +0330

    Added 4th unittest

--- Assignment 2/CryptoCurrency/CryptoCurrency.Tests/UnitTest1.cs
+++ Assignment 2/CryptoCurrency/CryptoCurrency.Tests/UnitTest1.cs
@@ -94,6 +94,41 @@ namespace CryptoCurrency.Tests
             Assert.Equal(expectedOutput, sw.ToString());
         }
 
-        
+        [Fact]
+        public void TestProcessOrders4()
+        {
+            // Arrange
+            var orderBookService = new OrderBookService();
+
+            var input = "10 1\ncoin_00\n11\n" +
+                        "10000 ADD order_001 sell coin_00 15009 8\n" +
+                        "10001 ADD order_002 buy coin_00 16057 5\n" +
+                        "10002 ADD order_003 sell coin_00 15024 8\n" +
+                        "10003 ADD order_004 buy coin_00 16125 9\n" +
+                        "10004 ADD order_005 sell coin_00 15074 6\n" +
+                        "10005 REM order_001 8\n" +
+                        "10006 ADD order_006 sell coin_00 15076 7\n" +
+                        "10007 REM order_005 6\n" +
+                        "10008 ADD order_007 buy coin_00 16184 7\n" +
+                        "10009 ADD order_008 sell coin_00 15141 7\n" +
+                        "10010 REM order_004 9\n";
+
+            // Create a StringWriter to capture the program's output
+            using StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+
+            // Act
+            Program.ProcessOrders(new StringReader(input));
+
+            // Assert
+            var expectedOutput = "10002 buy coin_00 150120.00\n" +
+                                 "10003 sell coin_00 161182.00\n" +
+                                 "10005 buy coin_00 150340.00\n" +
+                                 "10007 buy coin_00 150344.00\n" +
+                                 "10008 sell coin_00 161663.00\n" +
+                                 "10010 sell coin_00 161459.00\n";
+
+            Assert.Equal(expectedOutput, sw.ToString());
+        }
     }
 }
