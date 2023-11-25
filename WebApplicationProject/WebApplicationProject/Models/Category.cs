340b404aa0ae3c92ed22248da5d1bcabd2f149ea
Alireza Saei <saeialireza359@gmail.com>
2023-11-25 20:01:19 +0330

    Added models to project

---
+++ WebApplicationProject/WebApplicationProject/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplicationProject.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Category> Children { get; set; }
+    public Category Parent { get; set; }
+    public string Image { get; set; }
+    public List<Product> Products { get; set; }
+}
---
+++ WebApplicationProject/WebApplicationProject/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplicationProject.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public int Rating { get; set; }
+    public List<string> Colors { get; set; }
+    public List<string> Sizes { get; set; }
+    public List<string> Images { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int Inventory { get; set; }
+    public Category Category { get; set; }
+    public User User { get; set; }
+}
