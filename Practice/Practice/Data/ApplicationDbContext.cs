6c6efe6f53c71249f76b6dd0c725bfac859c4810
Alireza Saei <saeialireza359@gmail.com>
2023-12-07 00:33:20 +0330

    Created Dababase table for categories

---
+++ Practice/Practice/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practice.Models;
+
+namespace Practice.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Category>? Categories { set; get; }
+}
