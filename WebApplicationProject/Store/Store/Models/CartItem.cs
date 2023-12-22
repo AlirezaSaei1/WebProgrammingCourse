5479110f81a7823db64c7748a1fdef92557f7572
Alireza Saei <saeialireza359@gmail.com>
2023-12-22 15:46:42 +0330

    Added models

---
+++ WebApplicationProject/Store/Store/Models/CartItem.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplicationProject.Models;
+
+public class CartItem
+{
+    public int Id { get; set; }
+    public Product Product { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ WebApplicationProject/Store/Store/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplicationProject.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Category> Children { get; set; }
+    public Category Parent { get; set; }
+    public List<byte[]> Image { get; set; }
+    public List<Product> Products { get; set; }
+}
